4ead4173339c07bdfb59793c8ecde5dd18577f0f
hsingnchu <hsingnchu@gmail.com>
2023-01-06 15:36:37 +0800

    下架壓縮檔，上傳完整檔
    下架壓縮檔，上傳完整檔

---
+++ AjaxHomeWork_MSIT145_23林幸嫻/AjaxHomWork_MSIT145_23林幸嫻/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AjaxHomWork_MSIT145_23林幸嫻.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AjaxHomWork_MSIT145_23林幸嫻.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult 作業1()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AjaxHomeWork_MSIT145_23林幸嫻/AjaxHomWork_MSIT145_23林幸嫻/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AjaxHomWork_MSIT145_23林幸嫻.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
